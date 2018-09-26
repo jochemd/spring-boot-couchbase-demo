a896db8cf67ce151798bf2371d479ca6ce574d96
Jochem van Dieten <j.dieten@webellian.com>
2018-09-26 13:55:03 +0200

    Demo of SQL injection against Spring Data Couchbase

--- src/main/java/net/vandieten/couchbasedemo/repository/CarRepository.java
+++ src/main/java/net/vandieten/couchbasedemo/repository/CarRepository.java
@@ -1,9 +1,13 @@
 package net.vandieten.couchbasedemo.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import net.vandieten.couchbasedemo.entity.Car;
 
 public interface CarRepository extends CrudRepository<Car, String> {
+
+    List<Car> findByModelStartingWith(String model);
 
 }
--- src/test/java/net/vandieten/couchbasedemo/repository/CarRepositoryTest.java
+++ src/test/java/net/vandieten/couchbasedemo/repository/CarRepositoryTest.java
@@ -1,14 +1,21 @@
 package net.vandieten.couchbasedemo.repository;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.couchbase.core.CouchbaseTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.couchbase.client.core.CouchbaseException;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.query.N1qlQuery;
+import com.couchbase.client.java.query.N1qlQueryResult;
 
 import net.vandieten.couchbasedemo.entity.Car;
 
@@ -22,6 +29,15 @@ public class CarRepositoryTest {
     @Autowired
     CouchbaseTemplate template;
 
+    private String tstString1 = "Civic";
+    private String tstString2 = "C";
+    private String tstString3 = "C\" OR `model` LIKE \"";
+    
+    @Before
+    public void cleanUp() {
+        repo.deleteAll();
+    }
+    
     @Test
     public void crud() {
         Car car = getCar1();
@@ -37,6 +53,44 @@ public class CarRepositoryTest {
         assertFalse(repo.findById("my-first-car").isPresent());
     }
 
+    @Test
+    public void startingWith() {
+        Car car1 = getCar1();
+        repo.save(car1);
+        
+        Car car2 = getCar2();
+        repo.save(car2);
+
+        assertEquals(1, repo.findByModelStartingWith(tstString1).size());
+        assertEquals(1, repo.findByModelStartingWith(tstString2).size());
+        assertEquals(0, repo.findByModelStartingWith(tstString3).size());
+        
+    }
+
+    @Test
+    public void nativeN1QL() {
+        Car car1 = getCar1();
+        repo.save(car1);
+        
+        Car car2 = getCar2();
+        repo.save(car2);
+
+        assertEquals(1, runN1qlQuery(tstString1).allRows().size());
+        assertEquals(1, runN1qlQuery(tstString2).allRows().size());
+        assertEquals(0, runN1qlQuery(tstString3).allRows().size());
+        
+    }
+    
+    private N1qlQueryResult runN1qlQuery(String like) {
+        String statement = " SELECT * FROM demo car WHERE model LIKE $model ";
+        N1qlQuery query = N1qlQuery.parameterized(statement, JsonObject.create().put("model", like + "%"));
+        N1qlQueryResult result = template.queryN1QL(query);
+        if (!result.finalSuccess())
+            throw new CouchbaseException(result.errors().toString());
+        return result;
+    }
+    
+
     private Car getCar1() {
         Car car = new Car();
         car.setId("my-first-car");
@@ -45,5 +99,15 @@ public class CarRepositoryTest {
         car.setModel("Civic");
         return car;
     }
+    
+    private Car getCar2() {
+        Car car = new Car();
+        car.setId("my-second-car");
+        car.setBrand("Honda");
+        car.setYear(2008);
+        car.setModel("Accord");
+        return car;
+    }
+    
 
 }
